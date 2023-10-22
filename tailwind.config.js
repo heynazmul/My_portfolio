d366b03c747e589267b8769c2053f978890360af
heynazmul <heynazmulhossain@gmail.com>
2023-10-22 17:50:27 +0600

    my portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,15 @@ module.exports = {
   content: ["./public/**/*.{html,js,php}"],
   theme: {
     extend: {
-
+      fontFamily:{
+        inter: ["Inter", "sans-serif"],
+      },
       colors: {
         black : {
           100: '#1F1F1F'
         },
-        green : '#28E98C'
+        green : '#28E98C',
+        gray : '#656565'
       },
     },
   },
